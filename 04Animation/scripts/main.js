81852b23979da3b197a805d9161ae21bed5e85e0
maaikeroos <61278422+maaikeroos@users.noreply.github.com>
2020-05-18 15:47:49 +0200

    Changed rotationSpeed

--- 04Animation/scripts/main.js
+++ 04Animation/scripts/main.js
@@ -1,10 +1,10 @@
 //changing individual properties with code and using setInterval
- var rotationSpeed = 0.01;
+ var rotationSpeed = 0.7;
  var myOtherBox = document.getElementById('myOtherBox');
 
  function spin(){
- 	myOtherBox.object3D.rotation.y += rotationSpeed;
- 	console.log(myOtherBox.object3D.rotation.y);
+ 	myOtherBox.object3D.rotation.x += rotationSpeed;
+ 	console.log(myOtherBox.object3D.rotation.x);
  }
 
  setInterval(spin, 16); equivalent to 60 fps
